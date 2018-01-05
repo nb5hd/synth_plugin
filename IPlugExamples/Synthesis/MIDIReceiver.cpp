d18af0a9206f1b6a795b1957a65cbf809f532ce5
nb5hd <nb5hd@virginia.edu>
2018-01-04 19:28:26 -0500

    Added MIDI functionality

---
+++ IPlugExamples/Synthesis/MIDIReceiver.cpp
@@ -0,0 +1,8 @@
+//
+//  MIDIReceiver.cpp
+//  Synthesis
+//
+//  Created by Nikhil Bhaip on 1/1/18.
+//
+
+#include "MIDIReceiver.hpp"
---
+++ IPlugExamples/Synthesis/MIDIReceiver.h
@@ -0,0 +1,12 @@
+//
+//  MIDIReceiver.h
+//  Synthesis
+//
+//  Created by Nikhil Bhaip on 1/1/18.
+//
+
+#ifndef MIDIReceiver_h
+#define MIDIReceiver_h
+
+
+#endif /* MIDIReceiver_h */
