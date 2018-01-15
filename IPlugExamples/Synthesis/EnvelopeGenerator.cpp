b68c2d8d974cbb2b58c6b3b15c22b5735607a12f
nb5hd <nb5hd@virginia.edu>
2018-01-15 00:20:54 -0500

    Added ADSR envelope to synth

---
+++ IPlugExamples/Synthesis/EnvelopeGenerator.cpp
@@ -0,0 +1,8 @@
+//
+//  EnvelopeGenerator.cpp
+//  Synthesis
+//
+//  Created by Nikhil Bhaip on 1/12/18.
+//
+
+#include "EnvelopeGenerator.hpp"
---
+++ IPlugExamples/Synthesis/EnvelopeGenerator.h
@@ -0,0 +1,13 @@
+//
+//  EnvelopeGenerator.hpp
+//  Synthesis
+//
+//  Created by Nikhil Bhaip on 1/12/18.
+//
+
+#ifndef EnvelopeGenerator_hpp
+#define EnvelopeGenerator_hpp
+
+#include <stdio.h>
+
+#endif /* EnvelopeGenerator_hpp */
